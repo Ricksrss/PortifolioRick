d5c7dd2f620208bde703a995fc3b88e161950841
Ricksrss <henrique.hsoares@gmail.com>
2023-10-17 02:27:10 -0300

    2.1

--- portifolio/src/componentes/section/Presentation.js
+++ portifolio/src/componentes/section/Presentation.js
@@ -1,16 +1,20 @@
 import styles from './Presentation.module.css'
-
+import ButtonA from './elements/ButtonA'
+import ButtonB from './elements/ButtonB'
 function Presentation() {
     return (
         <div className={styles.presentation}>
             <h4><strong>Bem-vindo ao meu Portfólio</strong></h4>
-            <h1>Olá eu sou o Henrique Soares!</h1>
-            <p>Estou migrando de carreira e indo para área de programação,<br/>
-                 estudando para ser programador full-stack.<br/>
-                 Estou construindo minha carreira aos poucos sempre <br/>
-                 visando o crescimento profissional e pessoal.<br/>
-                 Atualmente estou estudando Analise e Desenvolvimento de<br/>
-                 Sistemas mas me formo em breve.</p>
+            <h1>Olá, eu sou o Henrique Soares!</h1>
+            <p>Estou migrando de carreira e indo para área de programação,<br />
+                estudando para ser programador full-stack.<br />
+                Estou construindo minha carreira aos poucos sempre <br />
+                visando o crescimento profissional e pessoal.<br />
+                Atualmente estou estudando Analise e Desenvolvimento de<br />
+                Sistemas mas me formo em breve.</p>
+
+            <ButtonA link='https://www.linkedin.com/in/henrique-soares-719819232/' text='Conecte-se comigo!' />
+            <ButtonB text='Botão B' />
         </div>
     )
 }
