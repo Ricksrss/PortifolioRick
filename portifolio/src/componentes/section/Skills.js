05219ec3519494ed3d88ffed315b97b1e07f27c3
Ricksrss <henrique.hsoares@gmail.com>
2023-10-17 03:12:15 -0300

    3.0

--- portifolio/src/componentes/section/Skills.js
+++ portifolio/src/componentes/section/Skills.js
@@ -1,9 +1,22 @@
-function Skills (){
+import styles from './Skills.module.css';
+import javascript from './image/skills/javascript.svg';
+import html from './image/skills/html.svg';
+import css from './image/skills/css.svg';
+import react from './image/skills/react.svg';
+
+function Skills() {
     return (
-        <div id="Skills">
-            <h1>Aqui ficará a minhas Skills</h1>
+        <div className={styles.skill} id="Skills">
+            <h1>Habilidades</h1>
+            <p>Tecnologias que estou explorando</p>
+            <div>
+                <img src={javascript} alt="JavaScript" />
+                <img src={html} alt="HTML" />
+                <img src={css} alt="CSS" />
+                <img src={react} alt="React" />
+            </div>
         </div>
-    )
+    );
 }
 
-export default Skills
+export default Skills;
