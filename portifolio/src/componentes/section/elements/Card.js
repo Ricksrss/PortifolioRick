d659673c572594a6ce244c669d7338c8f1c69605
Ricksrss <henrique.hsoares@gmail.com>
2023-10-18 03:40:00 -0300

    1a

--- portifolio/src/componentes/section/elements/Card.js
+++ portifolio/src/componentes/section/elements/Card.js
@@ -1,18 +1,37 @@
 import styles from './Card.module.css'
 import ButtonB from './ButtonB'
+import { useState } from 'react'
+import { func } from 'prop-types'
 
-function Card({img,title,tech,description,repo,site}){
-    return(
-        <div className={styles.card}>
-            <a href={site}>
-            <img src={img} alt ='ERROR'/>
+function Card({ img, title, tech, description, repo, site }) {
+
+    const [info, setInfo] = useState(false)
+
+    function infoOn() {
+        setInfo(true)
+    }
+
+    function infoOff(){
+        setInfo(false)
+    }
+
+
+    return (
+        <div onMouseLeave={infoOff} className={styles.card}>
+            <a onMouseEnter={infoOn}  href={site}>
+                <img src={img} alt='ERROR' />
             </a>
-           <section>
-            <h3>{title}</h3>
-            <p><strong>Tecnologia:</strong>{tech}</p>
-            <p>{description}</p>
-            <ButtonB text = 'Acesse o repositório' link={repo}/>
-           </section>
+
+            {info === true && (
+                <section>
+                    <h3>{title}</h3>
+                    <p><strong>Tecnologia:</strong>{tech}</p>
+                    <p>{description}</p>
+                    <ButtonB text='Acesse o repositório' link={repo} />
+                </section>
+
+            )}
+
         </div>
     )
 }
